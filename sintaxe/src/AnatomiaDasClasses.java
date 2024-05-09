8320a095d99c183aa0b38d1211b1715a74b60696
LoNe <taokentt@gmail.com>
2024-05-09 07:09:09 -0300

    anatomia das classes

---
+++ sintaxe/src/AnatomiaDasClasses.java
@@ -0,0 +1,15 @@
+//sintaxe e declaração de uma nova classe
+public class AnatomiaDasClasses {
+    public static void main(String[] args) throws Exception {
+        //corpo da classe
+        
+
+        // Declaração válida de veriáveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroum = 1;
+        int longo = 1;
+
+        System.out.println(numeroum);
+    }
+}
